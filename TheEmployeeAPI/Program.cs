81c2cc78fc0a23d4102a89e789dbf4e56be4b650
ericnkaka <nkakaeric96@gmail.com>
2025-03-04 17:00:52 +0200

    feat: adding employees endpoints and tests

--- TheEmployeeAPI/Program.cs
+++ TheEmployeeAPI/Program.cs
@@ -1,41 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+var employees = new List<Employee> {
+    new Employee {Id = 1, FirstName= "Eric", LastName="Nkaka"},
+    new Employee {Id = 2, FirstName= "Joe", LastName="Doe"}
+};
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "gApi documentation";
+    config.Title = "gAPI v1";
+    config.Version = "v1";
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var employeeRoute = app.MapGroup("employees");
+
 if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+{   
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "gAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+employeeRoute.MapGet(string.Empty, () => {
+   return Results.Ok(employees);
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+employeeRoute.MapGet("{id:int}", ([FromRoute] int id) => {
+    var employee = employees.SingleOrDefault(e => e.Id == id);
+    if(employee == null){
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+});
+
+employeeRoute.MapPost(string.Empty, ([FromBody]Employee employee, HttpContext context) => {
+    employee.Id = employees.Max(e => e.Id) + 1; 
+    employees.Add(employee);
+    return Results.Created($"employees/{employee.Id}", employee);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
