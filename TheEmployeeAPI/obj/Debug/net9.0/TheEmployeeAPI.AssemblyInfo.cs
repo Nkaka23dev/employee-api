87de54778ccf6c3ada7b8e024313afdcfc1cea20
ericnkaka <nkakaeric96@gmail.com>
2025-03-06 18:23:11 +0200

    chore: project setup

--- TheEmployeeAPI/obj/Debug/net9.0/TheEmployeeAPI.AssemblyInfo.cs
+++ TheEmployeeAPI/obj/Debug/net9.0/TheEmployeeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5c9ac52dc66eed65f63dc7a91c0cbdaf2c583c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3c42063e48c522587e700bacb7b5cc5f78e483")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheEmployeeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
