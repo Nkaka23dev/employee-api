c21d14ee5c2961a5124d3d30454385e52ebd7e79
ericnkaka <62412678+Nkaka23dev@users.noreply.github.com>
2025-04-08 20:20:45 +0200

    feat: Implementing methods to update user and to revoke refresh token (#10)
    * feat: implementing methods to update user and to revoke refresh token
    
    * feat: add global exception handler

--- TheEmployeeAPI/Services/User/IUserService.cs
+++ TheEmployeeAPI/Services/User/IUserService.cs
@@ -8,8 +8,8 @@ public interface IUserServices
   Task<UserResponse> LoginHandler(UserLoginRequest request);
   Task<CurrentUserResponse> GetCurrentUser();
   Task<UserResponse> GetUserById(Guid id);
-  Task<UserResponse> UpdateUser(Guid id, UpdatedUserRequest updatedUserRequest);
+  Task<UserResponse> UpdateUser(Guid id, UpdatedUserRequest request);
   Task DeleteUser(Guid id);
   Task<RevokeRefreshTokenResponse> RevokeRefreshToken(RefreshTokenRequest request);
-  Task<CurrentUserResponse> RefreshAccessToken(RefreshTokenRequest refreshTokenRequest);
+  Task<CurrentUserResponse> RefreshAccessToken(RefreshTokenRequest request);
 }
