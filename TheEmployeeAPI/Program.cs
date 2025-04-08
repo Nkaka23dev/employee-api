aeb6016b842ba75f80ccc3985d955cb52368acdb
ericnkaka <62412678+Nkaka23dev@users.noreply.github.com>
2025-04-08 20:24:51 +0200

    Feat/add configuration extensions (#11)
    * feat: implementing methods to update user and to revoke refresh token
    
    * feat: add global exception handler
    
    * feat: implements global configuration in applicationService extension
    
    * feat: add configuration of cors, jwt and identity
    
    * feat: add configuration of cors, jwt and identity

--- TheEmployeeAPI/Program.cs
+++ TheEmployeeAPI/Program.cs
@@ -2,15 +2,19 @@ using TheEmployeeAPI;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication;
+using TheEmployeeAPI.Exceptions;
+using Microsoft.OpenApi.Models;
+using TheEmployeeAPI.Entities.Auth;
+using Microsoft.AspNetCore.Identity;
+using TheEmployeeAPI.Services.User;
+using TheEmployeeAPI.Services;
+using TheEmployeeAPI.Infrastructure.MappingProfile;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TheEmployeeAPI.xml"));
-});
+
 builder.Services.AddProblemDetails();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();  
 builder.Services.AddControllers(options => {
@@ -21,14 +25,66 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<AppDbContext>(
     option => {
         option.UseSqlite(builder.Configuration.GetConnectionString("Default Connection"));
-        // option.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     } 
 );
+
+// builder.Services.AddIdentity<ApplicationUser, IdentityRole>();
+
+// builder.Services.AddScoped<IUserServices, UserService>();
+// builder.Services.AddScoped<ITokenService, TokenService>();
+// builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJwt(builder.Configuration);
+builder.Services.ConfigureCors();
+ 
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 builder.Services.AddSwaggerDocument();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+     new OpenApiInfo 
+     {
+        Title = "Employee API",
+        Version = "V1", 
+        Description="Provides basic employee management features"
+     });
+    
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Please Enter a valid Token in the following format: {Your token here} do not add word Bearer before it."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>() 
+        }
+    });
+
+    
+});
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
-{   
+{    
     app.UseSwagger();
        app.UseSwaggerUI(c =>
     {
@@ -41,6 +97,8 @@ using (var scope = app.Services.CreateScope()){
     var services = scope.ServiceProvider;
     SeedData.MigrateAndSeed(services);
 }
+app.UseCors("corsPolicy");
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
