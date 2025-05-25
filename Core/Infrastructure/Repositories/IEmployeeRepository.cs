22abd4510ac204fc796bc50536626a6fb78a8d0a
ericnkaka <nkakaeric96@gmail.com>
2025-05-25 15:46:57 +0200

    feat: generated database schama docus

--- Core/Infrastructure/Repositories/IEmployeeRepository.cs
+++ Core/Infrastructure/Repositories/IEmployeeRepository.cs
@@ -1,14 +1,15 @@
 using TheEmployeeAPI.Domain.Entities;
 
-namespace TheEmployeeAPI.Persistance.Repositories;
-
-public interface IEmployeeRepository
+namespace Core.Infrastructure.Repositories
 {
-    IQueryable<Employee> GetQuery(int? numberOfRecord = null, int? page = null);
-    Task AddAsync(Employee employee);
-    Task UpdateAsync(Employee employee);
-    Task<Employee?> GetByIdAsync(int id);
-    Task DeleteAsync(int id);
-    Task<Employee> GetBenefits(int id);
+    public interface IEmployeeRepository
+    {
+        // IQueryable<Employee> GetQuery(int? numberOfRecord = null, int? page = null);
+        // Task AddAsync(Employee employee);
+        // Task UpdateAsync(Employee employee);
+        // Task<Employee?> GetByIdAsync(int id);
+        // Task DeleteAsync(int id);
+        Task<Employee> GetBenefits(int id);
 
+    }
 }
