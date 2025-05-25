fdf646400ff21ebafea78dd616186975fc077c7a
ericnkaka <nkakaeric96@gmail.com>
2025-05-25 12:55:43 +0200

    feat: added mew service for shared functionalities

--- Core/Migrations/AppDbContextModelSnapshot.cs
+++ Core/Migrations/AppDbContextModelSnapshot.cs
@@ -2,21 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TheEmployeeAPI.Infrastructure.DbContexts;
 
 #nullable disable
 
-namespace TheEmployeeAPI.Migrations
+namespace Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250521145851_InitialCreate")]
-    partial class InitialCreate
+    partial class AppDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
