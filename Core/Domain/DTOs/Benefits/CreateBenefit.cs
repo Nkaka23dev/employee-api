a09ca1e9e5686b5decc911b8b859dc37dbf163e9
ericnkaka <62412678+Nkaka23dev@users.noreply.github.com>
2025-05-26 19:06:38 +0200

    WIP: benefit proxy changes continued (#35)

--- Core/Domain/DTOs/Benefits/CreateBenefit.cs
+++ Core/Domain/DTOs/Benefits/CreateBenefit.cs
@@ -1,8 +1,8 @@
-using System;
-
 namespace Core.Domain.DTOs.Benefits;
 
 public class CreateBenefit
 {
-
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required decimal BaseCost { get; set; }
 }
